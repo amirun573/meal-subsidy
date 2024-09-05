5be326f7db2b66421c223492d99b134ba74cdae7
MOHD AMIRUN AQMAR BIN ROSLAN <amirunaqmar@gmail.com>
2024-09-05 20:35:20 +0800

    make it small button

--- web/src/Components/Navbar/index.tsx
+++ web/src/Components/Navbar/index.tsx
@@ -57,13 +57,13 @@ const Navbar = ({ role }: any) => {
           <button
             onClick={toggleNavbar}
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-1 w-8 h-8 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-expanded={isOpen ? "true" : "false"}
             aria-controls="navbar-default"
           >
-            <span className="sr-only">Open main menu</span>
+            {/* <span className="sr-only">Open main menu</span> */}
             <svg
-              className="w-5 h-5"
+              className="w-4 h-4"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -77,6 +77,7 @@ const Navbar = ({ role }: any) => {
               />
             </svg>
           </button>
+
           <div
             className={`w-full md:flex md:items-center md:w-auto ${isOpen ? "block" : "hidden"}`}
             id="navbar-default"
