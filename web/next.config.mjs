68d4db5e4aa47abd8bb27026b056eb77856aeae9
MOHD AMIRUN AQMAR BIN ROSLAN <amirunaqmar@gmail.com>
2024-09-05 20:18:32 +0800

    update code

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,32 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: [
-            'flowbite.com',
-        ],
-    },
-    reactStrictMode: false,
-    webpack: (config, { isServer }) => {
-        // Aliasing `handlebars` to use the browser-friendly version
-        config.resolve.alias['handlebars'] = 'handlebars/dist/handlebars.min.js';
-
-        if (!isServer) {
-            // Ensure server-side only modules are not included in client-side bundle
-            config.resolve.fallback = {
-                fs: false,
-                module: false,
-                process: false,
-            };
-        }
-
-        // Adding handlebars-loader for `.handlebars` files
-        config.module.rules.push({
-            test: /\.handlebars$/,
-            loader: 'handlebars-loader',
-        });
-
-        return config;
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
