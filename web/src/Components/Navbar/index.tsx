955d5f43918f76be9a065da28178afa405e8abfe
MOHD AMIRUN AQMAR BIN ROSLAN <amirunaqmar@gmail.com>
2024-09-05 20:47:59 +0800

    cdfdf

--- web/src/Components/Navbar/index.tsx
+++ web/src/Components/Navbar/index.tsx
@@ -84,7 +84,7 @@ const Navbar = ({ role }: any) => {
                 <li key={item.id}>
                   <Link
                     href={item.link}
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    className="block py-2 px-3 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
                     {item.name}
                   </Link>
