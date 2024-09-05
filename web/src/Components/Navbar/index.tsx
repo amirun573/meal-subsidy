348e915765d3a6083fc0c709ca8f45cc92b572ef
MOHD AMIRUN AQMAR BIN ROSLAN <amirunaqmar@gmail.com>
2024-09-05 20:52:15 +0800

    update code

--- web/src/Components/Navbar/index.tsx
+++ web/src/Components/Navbar/index.tsx
@@ -46,20 +46,20 @@ const Navbar = ({ role }: any) => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900 fixed top-0 left-0 right-0 z-50 shadow text-black">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">
               Meal Subsidy
             </span>
           </a>
           <button
             onClick={toggleNavbar}
             type="button"
-            className="inline-flex items-center p-1.5 w-9 h-9 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-1 w-8 h-8 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-expanded={isOpen ? "true" : "false"}
             aria-controls="navbar-default"
           >
             {/* <span className="sr-only">Open main menu</span> */}
             <svg
-              className="w-5 h-5"
+              className="w-4 h-4"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -74,7 +74,6 @@ const Navbar = ({ role }: any) => {
             </svg>
           </button>
 
-
           <div
             className={`w-full md:flex md:items-center md:w-auto ${isOpen ? "block" : "hidden"}`}
             id="navbar-default"
@@ -84,7 +83,7 @@ const Navbar = ({ role }: any) => {
                 <li key={item.id}>
                   <Link
                     href={item.link}
-                    className="block py-2 px-3 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
                     {item.name}
                   </Link>
