41a50fe9554655bdc64c56883ac0349562cf4a50
amirun573 <amirunaqmar@gmail.com>
2024-09-28 17:48:46 +0800

    update code

--- web/cron.ts
+++ web/cron.ts
@@ -1,28 +1,39 @@
-import cron from "node-cron";
-import axios from "axios";
-import { StatusAPICode } from "./src/_Common/enum/status-api-code.enum";
-import { encrypt } from "./src/_Common/function/Hashing";
+import cron from 'node-cron';
+import axios from 'axios';
+import {StatusAPICode} from './src/_Common/enum/status-api-code.enum';
+import {encrypt} from './src/_Common/function/Hashing';
 
 const triggerCredit = async () => {
   try {
-    console.log("Executing triggerCredit...");
-    const baseURL = process.env.API_BASE_URL || "http://localhost:3000"; // Use environment variable or default
+    console.log('Executing triggerCredit...');
+    const baseURL = process.env.API_BASE_URL || 'http://localhost:3000'; // Use environment variable or default
 
     const response = await axios.post(`${baseURL}/api/subsidy`, {
       [StatusAPICode.code]: StatusAPICode.CREATE_TRIGGER_SUBSIDY_CREDIT_CRON,
-      key: encrypt("TRIGGER_CREDIT"),
+      key: encrypt('TRIGGER_CREDIT'),
     });
 
-    console.log("Successfully Triggered Credit:", response.data);
+    console.log('Successfully Triggered Credit:', response.data);
+
+    const nowInMalaysia = new Date().toLocaleString('en-US', {
+      timeZone: 'Asia/Kuala_Lumpur',
+    });
+    console.log('Done at ===>', nowInMalaysia);
+
   } catch (error) {
-    console.error("Error in triggerCredit:", error);
+    console.error('Error in triggerCredit:', error);
   }
 };
 
-// Schedule the cron job to run every 15 seconds (adjust for production use)
-cron.schedule("*/15 * * * * *", triggerCredit, {
+cron.schedule('48 17 * * *', triggerCredit, {
   scheduled: true,
-  timezone: "Asia/Kuala_Lumpur",
+  timezone: 'Asia/Kuala_Lumpur',
 });
 
-console.log("Cron job scheduled");
+// Schedule the cron job to run every 15 seconds (adjust for production use)
+// cron.schedule("*/15 * * * * *", triggerCredit, {
+//   scheduled: true,
+//   timezone: "Asia/Kuala_Lumpur",
+// });
+
+console.log('Cron job scheduled');
