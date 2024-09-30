5bed25b7dd89ae5ab2ec3c1cc8d622a62697b76d
MOHD AMIRUN AQMAR BIN ROSLAN <amirunaqmar@gmail.com>
2024-09-30 11:29:14 +0800

    update code

--- web/cron.ts
+++ web/cron.ts
@@ -25,7 +25,7 @@ const triggerCredit = async () => {
   }
 };
 
-cron.schedule('27 11 * * *', triggerCredit, {
+cron.schedule('50 07 * * *', triggerCredit, {
   scheduled: true,
   timezone: 'Asia/Kuala_Lumpur',
 });
