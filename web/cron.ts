723a2fed094e994125f7e8f898aaba1590295d38
MOHD AMIRUN AQMAR BIN ROSLAN <amirunaqmar@gmail.com>
2024-10-04 12:21:28 +0800

    update cronjob time for flushing and crediting

--- web/cron.ts
+++ web/cron.ts
@@ -25,7 +25,7 @@ const triggerCredit = async () => {
   }
 };
 
-cron.schedule('50 07 * * *', triggerCredit, {
+cron.schedule('50 05 * * *', triggerCredit, {
   scheduled: true,
   timezone: 'Asia/Kuala_Lumpur',
 });
